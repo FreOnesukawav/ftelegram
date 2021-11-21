d5a93b3bff26108c6f25c8d060fb8f475be9c298
Denver <csigorek@gmail.com>
2021-11-21 20:02:52 +0200

    refactor code

--- lib/screens/sampleScreen.dart
+++ lib/screens/sampleScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ftelegram/templates/basicText.dart';
+import 'package:ftelegram/templates/text/basicText.dart';
 
 class SampleScreen extends StatefulWidget {
   const SampleScreen({Key? key}) : super(key: key);
